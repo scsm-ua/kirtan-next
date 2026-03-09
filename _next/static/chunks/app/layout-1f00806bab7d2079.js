bdca9b6298fd07b53c4946339ddc364a12b54192
nadev03 <nadev03@users.noreply.github.com>
2026-03-09 21:28:31 +0000

    Deploying to gh-pages from @ scsm-ua/kirtan-next@db09fb0449cfceae81db9c8486d4e30611164a11 🚀

---
+++ _next/static/chunks/app/layout-1f00806bab7d2079.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{5099:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9081,23))},9081:()=>{}},e=>{e.O(0,[754,441,794,358],()=>e(e.s=5099)),_N_E=e.O()}]);
