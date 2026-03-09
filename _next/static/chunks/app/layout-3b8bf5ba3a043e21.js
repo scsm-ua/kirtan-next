93037fa5b62477ada0985d1314faadd16e21afe7
kostyanet <kostyanet@users.noreply.github.com>
2026-03-09 20:42:13 +0000

    Deploying to gh-pages from @ scsm-ua/kirtan-next@c84671be7ce0af7f041640630534651dec3bc891 🚀

---
+++ _next/static/chunks/app/layout-3b8bf5ba3a043e21.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7177],{5099:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9081,23))},9081:()=>{}},e=>{e.O(0,[8754,8441,3794,7358],()=>e(e.s=5099)),_N_E=e.O()}]);
