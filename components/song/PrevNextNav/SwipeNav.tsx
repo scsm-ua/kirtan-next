cec73e2d2c95a153e6b0c70cad4464105b43373a
Kostya <net.kostya@gmail.com>
2026-03-14 12:36:36 +0100

    Debug code #6

--- components/song/PrevNextNav/SwipeNav.tsx
+++ components/song/PrevNextNav/SwipeNav.tsx
@@ -1,6 +1,7 @@
 'use client';
-import SwipeListener from 'swipe-listener';
+// import SwipeListener from 'swipe-listener';
 import { useEffect } from 'react';
+import VanillaSwipe, { type EventData } from 'vanilla-swipe';
 
 import type { TNavItems } from '@/types/song';
 
@@ -13,23 +14,34 @@ type Props = { prevNext: TNavItems };
 export function SwipeNav({ prevNext }: Props) {
   let listener;
 
-  const goAway = ({ detail }: CustomEvent) => {
-    if (detail['directions'].left && prevNext.next) {
+  const goAway = ({ detail }: CustomEvent, data: EventData) => {
+    // if (detail['directions'].left && prevNext.next) {
+    if (data.directionX === 'LEFT' && prevNext.next) {
       window.location.href = prevNext.next.path;
     }
 
-    if (detail['directions'].right && prevNext.prev) {
+    // if (detail['directions'].right && prevNext.prev) {
+    if (data.directionX === 'RIGHT' && prevNext.prev) {
       window.location.href = prevNext.prev.path;
     }
   };
 
   useEffect(() => {
     const el = document.getElementById('body');
-    listener = SwipeListener(el);
-    el.addEventListener('swipe', goAway);
+    // listener = SwipeListener(el);
+    // el.addEventListener('swipe', goAway);
+    listener = new VanillaSwipe({
+      element: el,
+      // onSwiping: handler,
+      onSwiped: goAway,
+      mouseTrackingEnabled: true,
+    });
+
+    listener.init();
 
     return () => {
-      listener?.off();
+      // listener?.off();
+      listener?.destroy();
       el.removeEventListener('swipe', goAway);
     };
   }, [prevNext]);
