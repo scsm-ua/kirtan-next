a7e32c244add00911d399466b201af5ad08ac7d2
nadev03 <142786072+nadev03@users.noreply.github.com>
2026-06-06 16:54:26 +0300

    service worker  auto version

--- types/global.d.ts
+++ types/global.d.ts
@@ -2,3 +2,10 @@ declare module '*.scss' {
   const content: Record<string, string>;
   export default content;
 }
+
+declare const __BUILD_INFO__: {
+  branch: string;
+  commit: string;
+  builtTime: string;
+  version: string;
+};
